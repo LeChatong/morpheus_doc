037e33502de5fbc4bcc98a5c9741ed1be3441080
LeChatong <ulrich.tchatong@gmail.com>
2018-11-05 14:54:13 +0100

    First Commit

--- morpheus_doc/settings.py
+++ morpheus_doc/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yehd+=_bukt(ga-)+d9#jly_+d0j(5se=sx!n&h6iz(sjgo3n='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
